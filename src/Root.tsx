f4a6fe52c2c43d78d57bba1e2f9816bdc36ad346
Yurii Oliiarnyk <oliiarnyk14@gmail.com>
2022-05-16 19:23:19 +0300

    ✨ Added sign in screen

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,19 +1,31 @@
 import React, { useState } from "react";
-import SignInOptions from "./pages/SignInOptions";
-import SignUp from "./pages/SignUp";
+import SignInScreen from "./screens/SignInScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+
+enum Screens {
+  SIGN_IN = "sign-in",
+  SIGN_IN_EMAIL = "sign-in-email",
+  SIGN_UP_EMAIL = "sing-up-email",
+}
 
 const Root = () => {
-  const [signUp, setSingUp] = useState(false);
+  const [page, setPage] = useState(Screens.SIGN_IN);
 
-  const signUpWithEmail = () => {
-    setSingUp(true);
-  };
-
-  if (signUp) {
-    return <SignUp back={() => setSingUp(false)} />;
+  if (page === Screens.SIGN_UP_EMAIL) {
+    return <SignUpScreen back={() => setPage(Screens.SIGN_IN)} />;
   }
 
-  return <SignInOptions signUpWithEmail={signUpWithEmail} />;
+  if (page === Screens.SIGN_IN_EMAIL) {
+    return <SignInScreen back={() => setPage(Screens.SIGN_IN)} />;
+  }
+
+  return (
+    <WelcomeScreen
+      signUpWithEmail={() => setPage(Screens.SIGN_UP_EMAIL)}
+      signInWithEmail={() => setPage(Screens.SIGN_IN_EMAIL)}
+    />
+  );
 };
 
 export default Root;
---
+++ src/screens/SignUpScreen.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import Button from "../components/Button";
+import FormItem from "../components/FormItem";
+import Header from "../components/Header";
+import Input from "../components/Input";
+import AuthLayout from "../layouts/AuthLayout";
+import { getResponsiveSize } from "../utils";
+
+const SignUpScreen = ({ back }: { back: () => void }) => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  return (
+    <AuthLayout header={<Header title="Sign up with Email" back={back} />}>
+      <FormItem label="Your name">
+        <Input value={name} onChange={setName} />
+      </FormItem>
+      <FormItem label="Email">
+        <Input value={email} onChange={setEmail} />
+      </FormItem>
+      <FormItem last label="Password" helper="(min 6 characters)">
+        <Input value={password} onChange={setPassword} />
+      </FormItem>
+      <View style={styles.buttonView}>
+        <Button>SIGN UP</Button>
+      </View>
+    </AuthLayout>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonView: {
+    alignItems: "center",
+    marginTop: getResponsiveSize(26),
+  },
+});
+
+export default SignUpScreen;
